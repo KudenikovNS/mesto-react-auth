f0e50c3d0ba72a6cbc4b4dd747755644a0d4edc4
Nikita Kudenikov <KudenikovNS@yandex.ru>
2022-04-25 22:07:41 +0400

    №9_ Item 6_API connection

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,38 @@
+import React from "react";
+import { api } from "../utils/Api";
+
 function Main(props) {
+  const [userName, setUserName] = React.useState();
+  const [userDescription, setUserDescription] = React.useState();
+  const [userAvatar, setUserAvatar] = React.useState();
+
+  React.useEffect(() => {
+    api.getProfile().then((data) => {
+      setUserName(data.name);
+      setUserDescription(data.about);
+      setUserAvatar(data.avatar);
+    });
+  }, []);
+
   return (
     <main className="content">
       <div className="profile">
         <div onClick={props.onEditAvatar} className="profile__container-avatar">
-          <img
+          <div
+            className="profile__avatar"
+            style={{ backgroundImage: `url(${userAvatar})` }}
+          ></div>
+          {/*<img
             className="profile__avatar"
             src="<%=require('./images/avatar.jpg')%>"
             alt="Аватарка"
-          />
+          /> */}
           <div className="profile__change-avatar"></div>
         </div>
         <div className="profile__list">
           <div className="profile__info">
-            <h1 className="profile__title">Жак-Ив Кусто</h1>
-            <p className="profile__subtitle">Исследователь океана</p>
+            <h1 className="profile__title">{userName}</h1>
+            <p className="profile__subtitle">{userDescription}</p>
           </div>
           <button
             onClick={props.onEditProfile}
---
+++ src/utils/Api.js
@@ -0,0 +1,82 @@
+export class Api {
+  constructor({ baseUrl, headers }) {
+    this._headers = headers;
+    this._baseUrl = baseUrl;
+  }
+
+  _requestResult(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  }
+
+  getProfile() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then((res) => this._requestResult(res));
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    }).then((res) => this._requestResult(res));
+  }
+
+  updateAvatar(avatar) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar,
+      }),
+    }).then((res) => this._requestResult(res));
+  }
+
+  editProfile(name, about) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then((res) => this._requestResult(res));
+  }
+
+  addCard(name, link) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    }).then((res) => this._requestResult(res));
+  }
+
+  deleteConfirmCard(id) {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then((res) => this._requestResult(res));
+  }
+
+
+
+  addLikes(id) {
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then((res) => this._requestResult(res));
+  }
+}
+
+export const api = new Api({
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-39",
+  headers: {
+    authorization: "c930c17c-e4b0-4c5d-8837-f564c1b2701c",
+    "Content-Type": "application/json",
+  },
+});
